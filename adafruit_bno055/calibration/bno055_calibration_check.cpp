af01e8f0a26f26673c1a781b0fa068487feebef5
Furhad <furhadjidda@gmail.com>
2025-05-06 23:00:52 -0500

    updating the calibration check method

--- adafruit_bno055/calibration/bno055_calibration_check.cpp
+++ adafruit_bno055/calibration/bno055_calibration_check.cpp
@@ -6,40 +6,48 @@
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 int main() {
     stdio_init_all();
     printf("starting driver\n");
-    // Note to self , create this object on heap. creating on stack causes the
-    // program to crash
-    sleep_ms(1000);  // Wait 1 second for the system to stabilize
+
+    // Wait 1 second for the system to stabilize
+    sleep_ms(1000);
+
+    // Create BNO055 object on heap
     bno055_sensor::Bno055 *bno055 = new bno055_sensor::Bno055();
     bno055->initialization();
+    bno055->set_ext_crystal_use(true);
+    // Load calibration data from flash
     FlashManager flash_manager;
     CalibrationData calibration_data = {};
     flash_manager.read_data(calibration_data, sizeof(calibration_data));
+    // Wait for sensor to stabilize
+    sleep_ms(2000);
     bno055->set_calibration_data(calibration_data);
-    sleep_ms(5000);  // Wait 1 second for the sensor to stabilize
-    bool once = false;
-    while (true) {
-        int8_t temp = bno055->get_temp();
-        printf("Temperature: %d°C\n", temp);
+    sleep_ms(2000);
 
-        // System Status
-        if (!once) {
-            uint8_t system = 0;
-            uint8_t seltest = 0;
-            uint8_t error = 0;
+    // Print firmware version once
+    uint8_t system = 0;
+    uint8_t seltest = 0;
+    uint8_t error = 0;
+    bno055->get_system_status(&system, &seltest, &error);
+    printf("system: %x self test %x error %x \n", system, seltest, error);
+    bno055->check_firmware_version();
+
+    // Main loop
+    absolute_time_t last_status_time = get_absolute_time();
+    while (true) {
+        // Check if 2 seconds have passed
+        if (absolute_time_diff_us(last_status_time, get_absolute_time()) >= 2 * 1000 * 1000) {
             bno055->get_system_status(&system, &seltest, &error);
-            printf("system: %x selft test %x error %x \n", system, seltest, error);
-            bno055->check_firmware_version();
-            sleep_ms(500);  // Wait 500 ms
-            once = true;
+            printf("system: %x self test %x error %x \n", system, seltest, error);
+            last_status_time = get_absolute_time();
+            int8_t temp = bno055->get_temp();
+            printf("Temperature: %d°C\n", temp);
+            if (bno055->is_fully_calibrated()) {
+                printf("system fully calibrated\n");
+                bno055->get_calibration_data(calibration_data);
+            }
         }
 
         // Getting IMU data
